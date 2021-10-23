0336dfcc602276b2c404e64b5a738924ca740e36
Ganesh K <ganeshk.work@gmail.com>
2021-10-23 18:25:46 +0530

    add color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,13 @@ module.exports = {
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'regal-blue': 'teal'
+            }
+        }
     },
+
     variants: {},
     plugins: [],
 }
