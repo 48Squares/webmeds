7f8502af646e9cd40fe3d393bb2c0e7ce7e3d241
Ganesh K <ganeshk.work@gmail.com>
2021-11-06 11:12:46 +0530

    add dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,21 +1,17 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!doctype html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    {{--    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">--}}
-    <link rel="stylesheet" href="/dashboard/css/app.css">
-    <style>
-        body {
-            font-family: 'Nunito', sans-serif;
-        }
-    </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="./img/fav.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://kit-pro.fontawesome.com/releases/v5.12.1/css/pro.min.css">
+    <link rel="stylesheet" type="text/css" href="/dashboard/css/style.css">
+    <title>Welcome To Cleopatra</title>
 </head>
-<body class="antialiased">
-<div id="app"></div>
+<body class="bg-gray-100">
+<div id="app">
+
+</div>
 <script src="/dashboard/js/app.js"></script>
 </body>
 </html>
--- resources/views/dashboard/js/app.js
+++ resources/views/dashboard/js/app.js
@@ -7,7 +7,45 @@ import Me from "../Me";
 
 export default function App() {
     return (
-        <BrowserRouter>
+        @include('./base/start.html')
+
+
+        @include('./base/navbar.html')
+
+
+        <!-- strat wrapper -->
+    <div class="h-screen flex flex-row flex-wrap">
+
+        @include('./base/sidebar.html')
+
+        <!-- strat content -->
+    <div class="bg-gray-100 flex-1 p-6 md:mt-16">
+
+
+        <!-- General Report -->
+    @include('./index/generalReport.html')
+    <!-- End General Report -->
+
+    <!-- strat Analytics -->
+    @include('./index/analytics-1.html')
+    <!-- end Analytics -->
+
+    @include('./index/salesOverview.html')
+
+    @include('./index/numbers.html')
+
+    @include('./index/quickInfo.html')
+
+
+    </div>
+
+</div>
+
+
+
+@include('./base/end.html')
+
+<BrowserRouter>
             <Routes>
                 <Route path="test" element={<Dashboard />} > Dash </Route>
                 <Route path="/test/me" element={<Me />} />
