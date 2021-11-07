ebb8790bbb4d8c132f88313b026f2d4914d7ce6a
Ganesh K <ganeshk.work@gmail.com>
2021-11-07 06:28:47 +0530

    fix in route

--- resources/views/dashboard/js/app.js
+++ resources/views/dashboard/js/app.js
@@ -1,55 +1,28 @@
 import React from 'react';
 
 import "tailwindcss/tailwind.css";
-import Dashboard from "./Dashboard";
+import Dashboard from "./screen/Dashboard";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Me from "../Me";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Product from "./screen/Product";
 
 export default function App() {
     return (
-        @include('./base/start.html')
+        <>
+            <Navbar/>
+            <div className="h-screen flex flex-row flex-wrap">
+                <Sidebar/>
 
-
-        @include('./base/navbar.html')
-
-
-        <!-- strat wrapper -->
-    <div class="h-screen flex flex-row flex-wrap">
-
-        @include('./base/sidebar.html')
-
-        <!-- strat content -->
-    <div class="bg-gray-100 flex-1 p-6 md:mt-16">
-
-
-        <!-- General Report -->
-    @include('./index/generalReport.html')
-    <!-- End General Report -->
-
-    <!-- strat Analytics -->
-    @include('./index/analytics-1.html')
-    <!-- end Analytics -->
-
-    @include('./index/salesOverview.html')
-
-    @include('./index/numbers.html')
-
-    @include('./index/quickInfo.html')
-
-
-    </div>
-
-</div>
-
-
-
-@include('./base/end.html')
-
-<BrowserRouter>
-            <Routes>
-                <Route path="test" element={<Dashboard />} > Dash </Route>
-                <Route path="/test/me" element={<Me />} />
-            </Routes>
-        </BrowserRouter>
+                <div className="bg-gray-100 flex-1 p-6 md:mt-16">
+                    <BrowserRouter>
+                        <Routes>
+                            <Route path="test" element={<Dashboard/>} />
+                            <Route path="/test/product" element={<Product/>} />
+                        </Routes>
+                    </BrowserRouter>
+                </div>
+            </div>
+        </>
     );
 }
---
+++ resources/views/dashboard/js/screen/Product.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Product() {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur culpa cum doloribus, eos error
+            exercitationem, illum minus mollitia, odit sapiente sint sunt tenetur voluptatum. Asperiores, eligendi,
+            soluta. Commodi, corporis quam.
+        </div>
+    );
+}
